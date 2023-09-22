6d7af6c52942912de46105338d6ff85fcd255572
Maxime <maxime.marechalmccoy@gmail.com>
2023-09-22 14:56:45 -0700

    add schema tables

---
+++ db/schema/01_tables.sql
@@ -0,0 +1,49 @@
+-- Drop and recreate Users table (Example)
+DROP TABLE IF EXISTS users CASCADE;
+
+DROP TABLE IF EXISTS quizzes CASCADE;
+
+DROP TABLE IF EXISTS questions CASCADE;
+
+DROP TABLE IF EXISTS answers CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  avatar_url VARCHAR(255),
+  email VARCHAR(255)
+);
+
+CREATE TABLE quizzes (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) NOT NULL,
+  is_public BOOLEAN NOT NULL DEFAULT TRUE,
+  title VARCHAR(255) NOT NULL,
+  timestamp TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  quiz_id INTEGER REFERENCES quizzes(id) NOT NULL,
+  text TEXT NOT NULL
+);
+
+CREATE TABLE answers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  question_id INTEGER REFERENCES questions(id) NOT NULL,
+  text VARCHAR(255) NOT NULL,
+  is_correct BOOLEAN DEFAULT false
+);
+
+CREATE TABLE attempts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  quiz_id INTEGER REFERENCES quizzes(id) NOT NULL,
+  user_id INTEGER REFERENCES users(id) NOT NULL,
+  timestamp TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE answers_attempts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  answer_id INTEGER REFERENCES answers(id) NOT NULL,
+  attempt_id INTEGER REFERENCES attempts(id) NOT NULL,
+);
