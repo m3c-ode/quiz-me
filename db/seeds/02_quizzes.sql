a60a67a88da447cfd412f7da29e37ac7f9a91573
Maxime <maxime.marechalmccoy@gmail.com>
2023-09-22 15:29:20 -0700

    quizzes seed

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,10 +1,14 @@
 INSERT INTO
-  quizzes (owner_id, title) VALUE (1, 'Quizz 1')
+  quizzes (owner_id, title)
+VALUES
+  (1, 'Javascript');
+
 INSERT INTO
-  quizzes (owner_id, title) VALUE (1, 'Quizz 2')
+  quizzes (owner_id, title)
+VALUES
+  (2, 'Hockey');
+
 INSERT INTO
-  quizzes (owner_id, title) VALUE (3, 'Quizz 3')
-INSERT INTO
-  quizzes (owner_id, title) VALUE (2, 'Quizz 4')
-INSERT INTO
-  quizzes (owner_id, title) VALUE (2, 'Quizz 5')
+  quizzes (owner_id, title)
+VALUES
+  (1, 'Bloodbowl');
