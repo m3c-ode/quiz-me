6bf86e89b07dee824c78073ad982a6b66643e712
JT Smith <nexxai@gmail.com>
2023-09-23 20:50:16 -0600

    Adding the Attempts API

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use(express.json());
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
 const quizzesApiRoutes = require('./routes/quizzes-api');
+const attemptsApiRoutes = require('./routes/attempts-api');
 const usersRoutes = require('./routes/users');
 const { authMiddleware, router: authRoutes } = require('./routes/authentication');
 
@@ -51,6 +52,7 @@ const { authMiddleware, router: authRoutes } = require('./routes/authentication'
 // app.use('/api/users', authMiddleware, userApiRoutes);
 app.use('/api/users', userApiRoutes);
 app.use('/api/quizzes', quizzesApiRoutes);
+app.use('/api/attempts', attemptsApiRoutes);
 app.use('/users', usersRoutes);
 app.use('/auth', authRoutes);
 // Note: mount other resources here, using the same pattern above
