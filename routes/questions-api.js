a97fe8d129dc971a25295a34514c4a9bc63b2c63
hinali <hinali030@gmail.com>
2023-09-25 09:17:07 -0600

    added route to get a question by Id

--- routes/questions-api.js
+++ routes/questions-api.js
@@ -45,4 +45,72 @@ router.post('/', (req, res) => {
     });
 });
 
+// Route to get a question by ID
+router.get('/:id', (req, res) => {
+  const questionId = req.params.id;
+
+  // Ensure questionId is a positive integer
+  if (!Number.isInteger(+questionId) || +questionId <= 0) {
+    return res.status(400).json({ error: 'Invalid question ID.' });
+  }
+
+  // Query the database to retrieve the question
+  const queryString = `
+    SELECT * FROM questions
+    WHERE id = $1;
+  `;
+
+  const queryParams = [questionId];
+
+  dbQuery(queryString, queryParams)
+    .then((data) => {
+      if (data.length === 0) {
+        return res.status(404).json({ error: 'Question not found.' });
+      }
+
+      const question = data[0];
+      res.json({ question });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+// Route to update a question by ID
+router.patch('/:id', (req, res) => {
+  const questionId = req.params.id;
+  const { text } = req.body;
+
+  // Validate questionId and text, and handle errors if needed
+  if (!/^\d+$/.test(questionId) || parseInt(questionId) <= 0) {
+    return res.status(400).json({ error: 'Invalid question ID.' });
+  }
+
+  if (!text) {
+    return res.status(400).json({ error: 'Text is required for updating the question.' });
+  }
+
+  // Update the question in the database
+  const queryString = `
+    UPDATE questions
+    SET text = $1
+    WHERE id = $2
+    RETURNING *;
+  `;
+
+  const queryParams = [text, questionId];
+
+  dbQuery(queryString, queryParams)
+    .then((data) => {
+      const updatedQuestion = data[0];
+      if (!updatedQuestion) {
+        return res.status(404).json({ error: 'Question not found.' });
+      }
+      res.json({ question: updatedQuestion });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
 module.exports = router;
