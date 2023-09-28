1155d074909690ff406e6b3d38b7325e60a27f9f
Maxime <maxime.marechalmccoy@gmail.com>
2023-09-27 20:53:55 -0700

    fixed auth middleware

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const authMiddleware = (req, res, next) => {
-  if (!req.session.userId || !req.session.user) {
+  if (!req.session.user) {
     return res.status(401).json({ message: "Unauthorized" });
   }
   next();
@@ -10,14 +10,16 @@ const authMiddleware = (req, res, next) => {
 
 const authRedirectMiddleware = (req, res, next) => {
   //TODO:  Redirect to /login when ready
-  if (!req.session.userId || !req.session.user) {
+  if (!req.session.user) {
     return res.redirect("/");
   }
   next();
 };
 
 router.get("/login/:id", (req, res) => {
+  console.log("🚀 ~ file: authentication.js:21 ~ session pre-assignment:", req.session);
   req.session.user.id = req.params.id;
+  console.log("🚀 ~ file: authentication.js:23 ~ router.get ~ req.session:", req.session);
   res.redirect('/');
 });
 
